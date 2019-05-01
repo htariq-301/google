f1dd50aab5455b49114405b519ab6dcc296d2f4c
PATHAN <owaiskamal12@gmail.com>
2019-05-01 15:12:03 +0500

    Camera start taking picture

--- app/src/main/java/com/example/balouch/google/MainActivity.java
+++ app/src/main/java/com/example/balouch/google/MainActivity.java
@@ -1,37 +1,80 @@
 package com.example.balouch.google;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.speech.RecognizerIntent;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.TreeMap;
+//import com.hzitoun.camera2SecretPictureTaker.R;
+import com.example.balouch.google.PictureCapturingListener;
+import com.example.balouch.google.APictureCapturingService;
+import com.example.balouch.google.PictureCapturingServiceImpl;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PictureCapturingListener, ActivityCompat.OnRequestPermissionsResultCallback {
     private static final int REQUEST_CODE_SPEECH_INPUT = 1000;
     TextView TV;
     ImageView IV;
+    private static final String[] requiredPermissions = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA,
+    };
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_CODE = 1;
+
+    //The capture service
+    private APictureCapturingService pictureService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPermissions();
         TV = (TextView) findViewById(R.id.outText);
         IV = (ImageView)findViewById(R.id.Mic);
 
+        pictureService = PictureCapturingServiceImpl.getInstance(this);
         IV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Speak();
             }
         });
+
+        final Button btn = (Button) findViewById(R.id.btnpic);
+
+//        btn.setOnClickListener(v ->{
+//            showToast("Start Capture");
+//            pictureService.startCapturing(this);
+//        });
         }
 
+        public void Takepic(View view)
+        {
+            showToast("Start Capture");
+            pictureService.startCapturing(this);
+        }
+    private void showToast(final String text) {
+        runOnUiThread(() ->
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show()
+        );
+    }
     private void Speak() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
@@ -64,6 +107,67 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-}
 
+    @Override
+    public void onDoneCapturingAllPhotos(TreeMap<String, byte[]> picturesTaken) {
+        if (picturesTaken != null && !picturesTaken.isEmpty()) {
+            showToast("Done capturing all photos!");
+            return;
+        }
+        showToast("No camera detected!");
+    }
+
+    /**
+     * Displaying the pictures taken.
+     */
+    @Override
+    public void onCaptureDone(String pictureUrl, byte[] pictureData) {
+        if (pictureData != null && pictureUrl != null) {
+            runOnUiThread(() -> {
+                final Bitmap bitmap = BitmapFactory.decodeByteArray(pictureData, 0, pictureData.length);
+                final int nh = (int) (bitmap.getHeight() * (512.0 / bitmap.getWidth()));
+                final Bitmap scaled = Bitmap.createScaledBitmap(bitmap, 512, nh, true);
+                if (pictureUrl.contains("0_pic.jpg")) {
+                  //  uploadBackPhoto.setImageBitmap(scaled);
+                } else if (pictureUrl.contains("1_pic.jpg")) {
+                  //  uploadFrontPhoto.setImageBitmap(scaled);
+                }
+            });
+            showToast("Picture saved to " + pictureUrl);
+        }
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_CODE: {
+                if (!(grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    checkPermissions();
+                }
+            }
+        }
+    }
+
+    /**
+     * checking  permissions at Runtime.
+     */
+    @TargetApi(Build.VERSION_CODES.M)
+    private void checkPermissions() {
+        final List<String> neededPermissions = new ArrayList<>();
+        for (final String permission : requiredPermissions) {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                    permission) != PackageManager.PERMISSION_GRANTED) {
+                neededPermissions.add(permission);
+            }
+        }
+        if (!neededPermissions.isEmpty()) {
+            requestPermissions(neededPermissions.toArray(new String[]{}),
+                    MY_PERMISSIONS_REQUEST_ACCESS_CODE);
+        }
+    }
+}
 
